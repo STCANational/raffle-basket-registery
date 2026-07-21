3d6df01411a54b536eeb0d4ed86e8169dc8b314e
Cole Jaeger <jaegernw@gmail.com>
2026-07-21 15:25:26 -0400

    fixing header color

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,7 +1,7 @@
 const AppHeader: React.FC = () => {
   return (
     <header style={{ backgroundColor: 'rgb(194, 0, 18)', color: '#fff', padding: '1.5rem' }}>
-      <h1>STCA Gift Basket Registery</h1>
+      <h1 style={{color:'rgb(255,255,255)'}}>STCA Gift Basket Registery</h1>
     </header>
   );
 };
